b46ce98fc13c8c7d0aa78ac5ee6de436ed7d9a21
Yasin GÜL <89739419+yasingul@users.noreply.github.com>
2022-06-18 11:48:19 +0300

    NLayer API

---
+++ ThreeLayer.API/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ThreeLayer.Service;
+
+namespace ThreeLayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductService _productService;
+
+        public ProductController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var responses = await _productService.GetAll();
+            return new ObjectResult(responses) { StatusCode = responses.Status };
+        }
+    }
+}
